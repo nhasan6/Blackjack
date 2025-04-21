f1769a55c5f5a6757a68ba4724267cb6656dde2c
nhasan6 <169307919+nhasan6@users.noreply.github.com>
2025-04-21 19:04:54 -0400

    Add dependancies

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='blackjack',
-   version='0.1',
-   packages=find_packages(),  # Automatically finds src, etc.
+    name='blackjack',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "pygame-ce>=2.5.3"
+    ],
 )
