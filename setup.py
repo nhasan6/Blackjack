f3d3abc27874d90c90021125773b2cb4a95a021a
nhasan6 <169307919+nhasan6@users.noreply.github.com>
2025-04-21 18:25:35 -0400

    Change file structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Blackjack/setup.py
+from setuptools import setup, find_packages
+
+setup(
+   name='blackjack',
+   version='0.1',
+   packages=find_packages(),  # Automatically finds src, etc.
+)
